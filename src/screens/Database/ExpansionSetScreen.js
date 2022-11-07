407dd3a19df1713b9100dca8279607d6073f8e5b
Ming Shane Tong <mstong800@gmail.com>
2022-11-08 00:05:24 +1300

    Load data into expansion set page

--- src/screens/Database/ExpansionSetScreen.js
+++ src/screens/Database/ExpansionSetScreen.js
@@ -1,10 +1,26 @@
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Appbar, Provider } from 'react-native-paper';
 
-export default function SetScreen() {
-	return (
-		<View>
-			<Text>Set</Text>
-		</View>
-	);
+export default class ExpansionSetScreen extends React.Component {
+	constructor(props) {
+		super(props);
+		this.set = props.route.params;
+	}
+
+	componentDidMount() {
+		// set title of the header
+		this.props.navigation.setOptions({
+			title: this.set.name
+		});
+	}
+
+	render() {
+		return (
+			<View>
+				<Text>Set</Text>
+			</View>
+		);
+	}
+	
 }

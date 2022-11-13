96a3b00518b26e70eed961a5540becd00e60ea20
Ming Shane Tong <mstong800@gmail.com>
2022-11-13 14:35:56 +1300

    Changed class components to functional components

--- src/screens/Database/DatabaseScreen/ExpansionList.js
+++ src/screens/Database/DatabaseScreen/ExpansionList.js
@@ -1,41 +1,33 @@
+import { useNavigation } from '@react-navigation/native';
 import * as React from 'react';
 import { List, TouchableRipple } from 'react-native-paper';
 
-function ExpansionItem({ set, navigation }) {
-	const onPress = () => navigation.navigate("ExpansionSetScreen", set)
+function ExpansionItem({ set }) {
+	let navigation = useNavigation();
+	const onPress = () => navigation.navigate("ExpansionSetScreen", {set: set})
 	return (
-		<List.Item title={set.name} onPress={onPress}>
-		</List.Item>
+		<List.Item title={set.name} onPress={onPress} />
 	);
 }
 
-function ExpansionAccordion({ series, sets, navigation }) {
+function ExpansionAccordion({ series, sets }) {
 	return (
 		<List.Accordion
 			title={series}
 			left={props => <List.Icon {...props} icon="folder" />}
 		>
 			{sets.map((set, index) => 
-				<ExpansionItem 
-					key={index} 
-					set={set} 
-					navigation={navigation}  
-				/>
+				<ExpansionItem key={index} set={set} />
 			)}
 		</List.Accordion>
 	);
 }
 
-export default function ExpansionList({ sets, navigation }) {	
+export default function ExpansionList({ sets }) {
 	return (
 		<List.Section title="Expansion Sets">
 			{sets.map(([series, sets], index) => 
-				<ExpansionAccordion 
-					key={index} 
-					series={series} 
-					sets={sets}
-					navigation={navigation} 
-				/>
+				<ExpansionAccordion key={index} series={series} sets={sets} />
 			)}
 		</List.Section>
 	);
